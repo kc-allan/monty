ebf098f776c64a542835377a35b5d78cc02b88a4
allan <allancheruiyot003@gmail.com>
2023-03-24 00:06:16 -0700

    ALX SE

--- monty.h
+++ monty.h
@@ -78,7 +78,8 @@ stack_t *add_node(stack_t **stack, const int n);
 stack_t *queue_node(stack_t **stack, const int n);
 void free_stack(stack_t *stack);
 size_t print_stack(const stack_t *stack);
-int isascii(char c);
+int isascii(int c);
+
 void push(stack_t **stack, unsigned int line_cnt);
 void pall(stack_t **stack, unsigned int line_cnt);
 void pint(stack_t **stack, unsigned int line_cnt);
